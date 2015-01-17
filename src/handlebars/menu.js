d27ecddf92e7dbf4eda47bde29281151b4ac5f32
Kunuk Nykjær <kunukn@gmail.com>
2015-01-17 19:33:20 +0100

    upd json

--- src/handlebars/menu.js
+++ src/handlebars/menu.js
@@ -21,39 +21,111 @@
         ],
 
         mainNavigationItems: [{            
-            text: 'main text',
+            text: 'main text1',
             url: '#',
-            description: 'description text',
+            description: 'main description',
             children: [
             {                
-                text: 'main text level2',
+                text: 'main text 11',
                 url: '#',
-                description: 'description text',
+                description: 'main description 11',
+                 children: [
+                {                
+                    text: 'main text 111',
+                    url: '#',
+                    description: 'main description 111',
+                }
+                ]
             },
             {                
-                text: 'main2 text level2',
+                text: 'main text 12',
                 url: '#',
-                description: 'description text',
+                description: 'main description 12',
                 children: [
                 {                
-                    text: 'main text level3',
+                    text: 'main text 112',
                     url: '#',
-                    description: 'description text level 3',
+                    description: 'main description 112'
                 }
                 ]
             }
             ]
         }, {            
             text: 'main text2',
-            url: '#'
+            url: '#',
+             children: [
+            {                
+                text: 'main text 21',
+                url: '#',
+                description: 'main description 21',
+            },
+            {                
+                text: 'main text 22',
+                url: '#',
+                description: 'main description 22',
+                children: [
+                {                
+                    text: 'main text 212',
+                    url: '#',
+                    description: 'main description 212',
+                }
+                ]
+            }
+            ]
         }],
-        leftNavigationItems: [{            
-            text: 'left text',
-            url: '#'
-        }, {            
+
+        leftNavigationItems: [
+        {            
+            text: 'left text1',
+            url: '#',
+            children: [
+            {                
+                text: 'left text 11',
+                url: '#',                
+            },
+            {                
+                text: 'left text 12',
+                url: '#',                                
+            }
+            ]
+        }, 
+        {            
             text: 'left text2',
-            url: '#'
-        }]
+            url: '#',
+             children: [
+            {                
+                text: 'left text 21',
+                url: '#',                
+            },
+            {                
+                text: 'left text 22',
+                url: '#',                                
+            }
+            ]
+        },
+         {            
+            text: 'left text3',
+            url: '#'                      
+        },
+        {            
+            text: 'left text4',
+            url: '#'                      
+        },
+        {            
+            text: 'left text5',
+            url: '#',
+             children: [
+            {                
+                text: 'left text 51',
+                url: '#',                
+            },
+            {                
+                text: 'left text 52',
+                url: '#',                                
+            }
+            ]
+        }
+        ]
     };
 
     var template = Handlebars.compile($('#template').html());
