db94bc078741f84a45ef1d3fc78a8b5778617c11
Kunuk Nykjær <kunukn@gmail.com>
2015-01-18 16:45:51 +0100

    add toplinks rendering

--- src/handlebars/menu.js
+++ src/handlebars/menu.js
@@ -1,8 +1,9 @@
 ﻿var WEB = WEB || {};
 
 ! function(d, $) {    
-    var template = Handlebars.compile($('#templateSmallDeviceNavigation').html());
-    var html = template(WEB.stub);
-   $('#renderedSmallDeviceNavigation').append(html);
-
+    var templateSmallDeviceNavigation = Handlebars.compile($('#templateSmallDeviceNavigation').html());
+    var templateTopPanelGlobalLinks = Handlebars.compile($('#templateTopPanelGlobalLinks').html());
+    
+    $('#renderedSmallDeviceNavigation').append(templateSmallDeviceNavigation(WEB.stub));
+    $('#renderedTopPanelGlobalLinks').append(templateTopPanelGlobalLinks(WEB.stub));
 }(document, jQuery);
