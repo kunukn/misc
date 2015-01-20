5de1fdc7e2007a623fa74bf0ce1f39906024c182
Kunuk Nykjær <kunukn@gmail.com>
2015-01-20 23:06:17 +0100

    placeholders

--- src/handlebars/site-data.js
+++ src/handlebars/site-data.js
@@ -3,141 +3,161 @@
 (function() {
     Site = Site || {};
     Site.stub = {
-        globalNavigationItems: [
-        {
-            text: 'Events'
-            ,url: '#'
-        },
-        {
-            text: 'News & Media'
-            ,url: '#'
-        },
-        {
-            text: 'Contact'
-            ,url: '#'
-        },
-        {
-            text: 'About us'
-            ,url: '#'
-        }
-        ],
+
+        htmlSpots: {},
+        htmlSpotTitleDefault: 'Wood advisory, development and design',
+        htmlSpotBodyDefault: 'We are passionate people building We are passionate people building We are passionate people building We are passionate people building We are passionate people building Meet us here',
+        htmlSpotIconDefault: 'glyphicon glyphicon-headphones',
+        globalNavigationItems: [{
+            text: 'Events',
+            url: '#'
+        }, {
+            text: 'News & Media',
+            url: '#'
+        }, {
+            text: 'Contact',
+            url: '#'
+        }, {
+            text: 'About us',
+            url: '#'
+        }],
 
         mainNavigationItems: [{
             text: 'Industries & Solutions',
-            children: [
-            {
+            children: [{
                 text: 'Industries',
-                children: [
-                {
-                    text: 'IT sciences'
-                    ,url: '#'
-                    ,description: 'With profound domain knowledge, Wood knows how to use IT to optimize business processes while staying compliant'
-                },
-                 {
-                    text: 'Enterprise'
-                    ,url: '#'
-                    ,description: 'Wood has a proven track record of handling IT challenges, in terms of both size and complexity, for large enterprises'
-                }
-                ]
-            },
-            {
-                text: 'Solutions'
-                ,children: [
-                {
-                    text: 'Advisory Services'
-                    ,url: '#'
-                    ,description: 'Wood’s advisory services span a variety of areas and are built to optimize your company’s use of IT'
-                },
-                 {
-                    text: 'Application Design'
-                    ,url: '#'
-                    ,description: 'Based on best practices, we manage applications in a secure, flexible and scalable way, freeing you to focus on your core business'
-                },
-                 {
-                    text: 'Application Design2'
-                    ,url: '#'
-                    ,description: 'Based on best practices, we manage applications in a secure, flexible and scalable way, freeing you to focus on your core business'
-                },
-                 {
-                    text: 'Application Design3'
-                    ,url: '#'
-                    ,description: 'Based on best practices, we manage applications in a secure, flexible and scalable way, freeing you to focus on your core business'
-                },
-                  {
-                    text: 'Application Design4'
-                    ,url: '#'
-                    ,description: 'Based on best practices, we manage applications in a secure, flexible and scalable way, freeing you to focus on your core business'
-                }
-                ]
-            }
-            ]
+                children: [{
+                    text: 'IT sciences',
+                    url: '#',
+                    description: 'With profound domain knowledge, Wood knows how to use IT to optimize business processes while staying compliant'
+                }, {
+                    text: 'Enterprise',
+                    url: '#',
+                    description: 'Wood has a proven track record of handling IT challenges, in terms of both size and complexity, for large enterprises'
+                }]
+            }, {
+                text: 'Solutions',
+                children: [{
+                    text: 'Advisory Services',
+                    url: '#',
+                    description: 'Wood’s advisory services span a variety of areas and are built to optimize your company’s use of IT'
+                }, {
+                    text: 'Application Design',
+                    url: '#',
+                    description: 'Based on best practices, we manage applications in a secure, flexible and scalable way, freeing you to focus on your core business'
+                }, {
+                    text: 'Application Design2',
+                    url: '#',
+                    description: 'Based on best practices, we manage applications in a secure, flexible and scalable way, freeing you to focus on your core business'
+                }, {
+                    text: 'Application Design3',
+                    url: '#',
+                    description: 'Based on best practices, we manage applications in a secure, flexible and scalable way, freeing you to focus on your core business'
+                }, {
+                    text: 'Application Design4',
+                    url: '#',
+                    description: 'Based on best practices, we manage applications in a secure, flexible and scalable way, freeing you to focus on your core business'
+                }]
+            }]
         }, {
-            text: 'Customers & cases'
-            ,children: [
-            {
-                text: 'Cases'
-                ,url: '#'
-                ,description: 'main description 21'
-            },
-            {
-                text: 'Customers'
-                ,url: '#'
-                ,description: 'main description 22'
-            }
-            ]
+            text: 'Customers & cases',
+            children: [{
+                text: 'Cases',
+                url: '#',
+                description: 'main description 21'
+            }, {
+                text: 'Customers',
+                url: '#',
+                description: 'main description 22'
+            }]
         }],
 
-        leftNavigationItems: [
-        {
-            text: 'left text1'
-            ,url: '#'
-            ,children: [
-            {
-                text: 'left text 11'
-                ,url: '#'
-            },
-            {
-                text: 'left text 12'
-                ,url: '#'
-            }
-            ]
-        },
-        {
+        leftNavigationItems: [{
+            text: 'left text1',
+            url: '#',
+            children: [{
+                text: 'left text 11',
+                url: '#'
+            }, {
+                text: 'left text 12',
+                url: '#'
+            }]
+        }, {
             text: 'left text2',
             url: '#',
-             children: [
-            {
-                text: 'left text 21'
-                ,url: '#'
-            },
-            {
-                text: 'left text 22'
-                ,url: '#'
-            }
-            ]
-        },
-         {
-            text: 'left text3'
-            ,url: '#'
-        },
-        {
-            text: 'left text4'
-            ,url: '#'
-        },
-        {
+            children: [{
+                text: 'left text 21',
+                url: '#'
+            }, {
+                text: 'left text 22',
+                url: '#'
+            }]
+        }, {
+            text: 'left text3',
+            url: '#'
+        }, {
+            text: 'left text4',
+            url: '#'
+        }, {
             text: 'left text5',
             url: '#',
-            children: [
-            {
-                text: 'left text 51'
-                ,url: '#'
-            },
-            {
-                text: 'left text 52'
-                ,url: '#'
-            }
-            ]
-        }
-        ]
+            children: [{
+                text: 'left text 51',
+                url: '#'
+            }, {
+                text: 'left text 52',
+                url: '#'
+            }]
+        }]
     };
+
+    (function(stub) {
+        stub.htmlSpots = {
+            htmlSpot1: {
+                background: 'spot-background-turkish',
+                url: '#',
+                title: stub.htmlSpotTitleDefault,
+                icon: 'glyphicon glyphicon-headphones',
+                body: stub.htmlSpotBodyDefault
+            },
+            htmlSpot2: {
+                background: 'spot-background-gray',
+                title: stub.htmlSpotTitleDefault,
+                icon: 'glyphicon glyphicon-wrench',
+                body: stub.htmlSpotBodyDefault
+            },
+            htmlSpot3: {
+                background: 'spot-background-green',
+                title: stub.htmlSpotTitleDefault,
+                body: stub.htmlSpotBodyDefault
+            },
+            htmlSpot4: {
+                background: 'spot-background-gray',
+                title: stub.htmlSpotTitleDefault,
+                body: stub.htmlSpotBodyDefault
+            },
+            htmlSpot5: {
+                background: 'spot-background-turkish',
+                title: stub.htmlSpotTitleDefault,
+                body: stub.htmlSpotBodyDefault
+            },
+            htmlSpot6: {
+                background: 'spot-background-gray',
+                title: 'Responsive centered image',
+                body: 'We are passionate people'
+            },
+            htmlSpot7: {
+                background: 'spot-background-green',
+                title: stub.htmlSpotTitleDefault,
+                icon: 'glyphicon glyphicon-tags',
+                body: stub.htmlSpotBodyDefault
+            },
+            htmlSpot8: {
+                background: 'spot-background-red',
+                icon: 'glyphicon glyphicon-facetime-video',
+                title: stub.htmlSpotTitleDefault,
+                body: stub.htmlSpotBodyDefault
+            }
+        }
+    })(Site.stub);
 })();
--- src/handlebars/site.js
+++ src/handlebars/site.js
@@ -3,15 +3,31 @@
 (function(d, $) {
 	Site = Site || {};
 	$( document ).ready(function() {
-		// Format url
+		
 	    Handlebars.registerHelper('urlFormat', function(url) {
 	        return url ? url : '#';
 	    });
 
-	    var templateSmallDeviceNavigation = Handlebars.compile($('#templateSmallDeviceNavigation').html());
-	    var templateTopPanelGlobalLinks = Handlebars.compile($('#templateTopPanelGlobalLinks').html());
+	    Handlebars.registerHelper('iconFormat', function(icon) {
+	        return icon ? icon : '';
+	    });
 
+	    var 
+	    templateSmallDeviceNavigation = Handlebars.compile($('#templateSmallDeviceNavigation').html()),
+	    templateTopPanelGlobalLinks = Handlebars.compile($('#templateTopPanelGlobalLinks').html()),
+	    templateHtmlSpot = Handlebars.compile($('#templateHtmlSpot').html()),
+	    templateHtmlImageSpot = Handlebars.compile($('#templateHtmlImageSpot').html());;	    
+	    
     	$('#renderedSmallDeviceNavigation').append(templateSmallDeviceNavigation(Site.stub));
     	$('#renderedTopPanelGlobalLinks').append(templateTopPanelGlobalLinks(Site.stub));
+    	
+    	$('#placeholder1').append(templateHtmlSpot(Site.stub.htmlSpots.htmlSpot1));
+    	$('#placeholder2').append(templateHtmlSpot(Site.stub.htmlSpots.htmlSpot2));
+    	$('#placeholder3').append(templateHtmlSpot(Site.stub.htmlSpots.htmlSpot3));
+    	$('#placeholder4').append(templateHtmlSpot(Site.stub.htmlSpots.htmlSpot4));
+    	$('#placeholder5').append(templateHtmlSpot(Site.stub.htmlSpots.htmlSpot5));
+    	$('#placeholder6').append(templateHtmlImageSpot(Site.stub.htmlSpots.htmlSpot6));
+    	$('#placeholder7').append(templateHtmlSpot(Site.stub.htmlSpots.htmlSpot7));
+    	$('#placeholder8').append(templateHtmlSpot(Site.stub.htmlSpots.htmlSpot8));
 	});
 })(document, jQuery);
