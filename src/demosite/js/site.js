7fc780863130d8d7bc29946c62d0434db56ab3b0
Kunuk Nykjær <kunukn@gmail.com>
2015-02-04 18:21:54 +0100

    move nav to demo site

--- src/demosite/js/site.js
+++ src/demosite/js/site.js
@@ -2,58 +2,65 @@
 
 /* site.js */
 (function(d, $) {
-	Site = Site || {}; // global var
+    Site = Site || {}; // global var
 
-	Site.Api = {};
-	Site.Api.isDesktop = function(){
-		return $(document).width() >= 992;
-	};	
+    Site.Api = {};
+    Site.Api.isDesktop = function() {
+        return $(document).width() >= 992;
+    };
 
-	$( document ).ready(function() {			
-	    Handlebars.registerHelper('urlFormat', function(url) {
-	        return url ? url : '#';
-	    });
+    $(document).ready(function() {
+        Handlebars.registerHelper('urlFormat', function(url) {
+            return url ? url : '#';
+        });
 
-	    Handlebars.registerHelper('iconFormat', function(icon) {
-	        return icon ? icon : '';
-	    });
+        Handlebars.registerHelper('iconFormat', function(icon) {
+            return icon ? icon : '';
+        });
 
-	    var 
-	    templateLanguagesSupported = Handlebars.compile($('#templateLanguagesSupported').html()),
-	    templateNavigation = Handlebars.compile($('#templateNavigation').html()),
-	    templateTopPanelGlobalLinks = Handlebars.compile($('#templateTopPanelGlobalLinks').html()),
-	    templateHtmlSpot = Handlebars.compile($('#templateHtmlSpot').html()),
-	    templateHtmlImageSpot = Handlebars.compile($('#templateHtmlImageSpot').html());;	    
-	    
-	    // Content
-    	$('#placeholder1').append(templateHtmlSpot(Site.Stub.htmlSpots.htmlSpot1));
-    	$('#placeholder2').append(templateHtmlSpot(Site.Stub.htmlSpots.htmlSpot2));
-    	$('#placeholder3').append(templateHtmlSpot(Site.Stub.htmlSpots.htmlSpot3));
-    	$('#placeholder4').append(templateHtmlSpot(Site.Stub.htmlSpots.htmlSpot4));
-    	$('#placeholder5').append(templateHtmlSpot(Site.Stub.htmlSpots.htmlSpot5));
-    	$('#placeholder6').append(templateHtmlImageSpot(Site.Stub.htmlSpots.htmlSpot6));
-    	$('#placeholder7').append(templateHtmlSpot(Site.Stub.htmlSpots.htmlSpot7));
-    	$('#placeholder8').append(templateHtmlSpot(Site.Stub.htmlSpots.htmlSpot8));
+        var templateLanguagesSupported = Handlebars.compile($('#templateLanguagesSupported').html()),
+            //templateNavigation = Handlebars.compile($('#templateNavigation').html()),
+            templateTopPanelGlobalLinks = Handlebars.compile($('#templateTopPanelGlobalLinks').html()),
+            templateHtmlSpot = Handlebars.compile($('#templateHtmlSpot').html()),
+            templateHtmlImageSpot = Handlebars.compile($('#templateHtmlImageSpot').html());
 
-	    // UI
-	    $('#renderedLanguageSelection').append(templateLanguagesSupported(Site.Stub));
-    	$('#renderedNavigation').append(templateNavigation(Site.Stub));
-    	$('#renderedTopPanelGlobalLinks').append(templateTopPanelGlobalLinks(Site.Stub));
+        // Content
+        $('#placeholder1').append(templateHtmlSpot(Site.Stub.htmlSpots.htmlSpot1));
+        $('#placeholder2').append(templateHtmlSpot(Site.Stub.htmlSpots.htmlSpot2));
+        $('#placeholder3').append(templateHtmlSpot(Site.Stub.htmlSpots.htmlSpot3));
+        $('#placeholder4').append(templateHtmlSpot(Site.Stub.htmlSpots.htmlSpot4));
+        $('#placeholder5').append(templateHtmlSpot(Site.Stub.htmlSpots.htmlSpot5));
+        $('#placeholder6').append(templateHtmlImageSpot(Site.Stub.htmlSpots.htmlSpot6));
+        $('#placeholder7').append(templateHtmlSpot(Site.Stub.htmlSpots.htmlSpot7));
+        $('#placeholder8').append(templateHtmlSpot(Site.Stub.htmlSpots.htmlSpot8));
 
-		navigationIsOpenedByDefaultForDesktop();    	   	  
-    	leftNavigationIsAvailableExceptForFrontpage();
-	});
+        // UI
+        $('#renderedLanguageSelection').append(templateLanguagesSupported(Site.Stub));
+        //$('#renderedNavigation').append(templateNavigation(Site.Stub));
+        $('#renderedTopPanelGlobalLinks').append(templateTopPanelGlobalLinks(Site.Stub));
 
-	function navigationIsOpenedByDefaultForDesktop(){
-		if(Site.Api.isDesktop() ){    	
-    		$('#renderedNavigation').removeClass('collapse');
-    	} 
-	}
+        // navigationIsOpenedByDefaultForDesktop();    	   	  
+        // leftNavigationIsAvailableExceptForFrontpage();
 
-	function leftNavigationIsAvailableExceptForFrontpage(){
-		if($('body').attr('data-pagetype') !== 'frontpage'){
-			$('.left-navigation-container').css('display','block');
-		}
-	}
+        $('#navigationMegamenu .flex-item').on('mouseenter', function(e) {
+            $(this).addClass('open');
+        });
+
+        $('#navigationMegamenu .flex-item').on('mouseleave', function(e) {
+            $(this).removeClass('open');
+        });
+    });
+
+    function navigationIsOpenedByDefaultForDesktop() {
+        if (Site.Api.isDesktop()) {
+            $('#renderedNavigation').removeClass('collapse');
+        }
+    }
+
+    function leftNavigationIsAvailableExceptForFrontpage() {
+        if ($('.body').attr('data-pagetype') !== 'frontpage') {
+            $('.left-navigation-container').css('display', 'block');
+        }
+    }
 
 })(document, jQuery);
