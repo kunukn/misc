90dad080762bb1a3e92ad7a70b19f6d96fa14af6
Kunuk Nykjær <kunukn@gmail.com>
2015-02-06 20:20:30 +0100

    upd quartely

---
+++ src/quaterly/js/app/run-app.js
@@ -0,0 +1,65 @@
+var app;
+
+(function($) {
+
+    $(function() {
+        // init Isotope
+        var $container = $('#isotope-demo').isotope({
+            itemSelector: '.element-item',
+            layoutMode: 'masonry',
+            masonry: {
+                columnWidth: 210
+            }
+        });
+
+        // bind filter button click
+        $('#filters').on('click', 'button', function() {
+            var filterValue = $(this).attr('data-filter');
+            $container.isotope({
+                filter: filterValue
+            });
+        });
+        // change is-checked class on buttons
+        $('.button-group').each(function(i, buttonGroup) {
+            var $buttonGroup = $(buttonGroup);
+            $buttonGroup.on('click', 'button', function() {
+                $buttonGroup.find('.is-checked').removeClass('is-checked');
+                $(this).addClass('is-checked');
+            });
+        });
+    });
+
+    /*** Data structures ***/
+
+    /* Container app */
+    function AppViewModel(model) {
+        // Data
+        var self = this;
+        self.viewModel = model.viewModel;
+    };
+
+    /* View Model */
+    function DemoViewModel(model) {
+        // Data
+        var self = this;
+        self.title = model.title;
+        self.body = model.body;
+    };
+
+    /*** Populate data ***/
+
+    var stub = {
+        viewModel: {
+            title: 'hello',
+            body: 'world'
+        }
+    };
+
+    var appViewModel = new AppViewModel(stub);
+
+    /*** Render app ***/
+    var appViewModelObservables = ko.mapping.fromJS(appViewModel);
+    ko.applyBindings(appViewModelObservables);
+    app = appViewModelObservables;
+
+})(jQuery);
