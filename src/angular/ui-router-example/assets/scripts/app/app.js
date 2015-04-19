a09c87abba4600485f98d1bc784d4891f723e7ed
Kunuk Nykjær <kunukn@gmail.com>
2015-04-19 23:26:20 +0200

    minor

--- src/angular/ui-router-example/assets/scripts/app/app.js
+++ src/angular/ui-router-example/assets/scripts/app/app.js
@@ -1,71 +1,70 @@
-// app.js
-var routerApp = angular.module('routerApp', ['ui.router']);
+(function() {
 
-routerApp.config(function($stateProvider, $urlRouterProvider) {
+    var routerApp = angular.module('routerApp', ['ui.router']);
 
-    $urlRouterProvider.otherwise('/home');
+    routerApp.config(function($stateProvider, $urlRouterProvider) {
 
-    $stateProvider
+        $urlRouterProvider.otherwise('/home');
 
-    // HOME STATES AND NESTED VIEWS ========================================
-    .state('home', {
-        url: '/home',
-        templateUrl: 'partial-home.html'
-    })
+        $stateProvider
 
-    // nested list with custom controller
-    .state('home.list', {
-        url: '/list',
-        templateUrl: 'partial-home-list.html',
-        controller: function($scope) {
-            $scope.dogs = ['Bernese', 'Husky', 'Goldendoodle'];
-        }
-    })
+            .state('home', {
+            url: '/home',
+            templateUrl: 'partial-home.html'
+        })
 
-    // nested list with just some random string data
-    .state('home.paragraph', {
-        url: '/paragraph',
-        template: 'I could sure use a drink right now.'
-    })
-
-    .state('about', {
-        url: '/about',
-        views: {
-
-            // the main template will be placed here (relatively named)
-            '': {
-                templateUrl: 'partial-about.html'
-            },
-
-            // the child views will be defined here (absolutely named)
-            'columnOne@about': {
-                template: 'Look I am a column!'
-            },
-
-            // for column two, we'll define a separate controller 
-            'columnTwo@about': {
-                templateUrl: 'table-data.html',
-                controller: 'scotchController'
+        .state('home.list', {
+            url: '/list',
+            templateUrl: 'partial-home-list.html',
+            controller: function($scope) {
+                $scope.dogs = ['Bernese', 'Husky', 'Goldendoodle'];
             }
-        }
+        })
 
-    }); // closes $routerApp.config()
-});
+        .state('home.paragraph', {
+            url: '/paragraph',
+            template: 'I could sure use a drink right now.'
+        })
 
-// let's define the scotch controller that we call up in the about state
-routerApp.controller('scotchController', function($scope) {
+        .state('about', {
+            url: '/about',
+            views: {
 
-    $scope.message = 'test';
+                // the main template will be placed here (relatively named)
+                '': {
+                    templateUrl: 'partial-about.html'
+                },
 
-    $scope.scotches = [{
-        name: 'Macallan 12',
-        price: 50
-    }, {
-        name: 'Chivas Regal Royal Salute',
-        price: 10000
-    }, {
-        name: 'Glenfiddich 1937',
-        price: 20000
-    }];
+                // the child views will be defined here (absolutely named)
+                'columnOne@about': {
+                    template: 'Look I am a column!'
+                },
 
-});
+                // for column two, we'll define a separate controller 
+                'columnTwo@about': {
+                    templateUrl: 'table-data.html',
+                    controller: 'scotchController'
+                }
+            }
+
+        });
+    });
+
+    routerApp.controller('scotchController', function($scope) {
+
+        $scope.message = 'test';
+
+        $scope.scotches = [{
+            name: 'Macallan 12',
+            price: 50
+        }, {
+            name: 'Chivas Regal Royal Salute',
+            price: 10000
+        }, {
+            name: 'Glenfiddich 1937',
+            price: 20000
+        }];
+
+    });
+
+})();
