7192e1f799a4a0c27e385650a660e1bad0cebacf
Kunuk Nykjær <kunukn@gmail.com>
2015-06-28 12:34:17 +0200

    refactor drag drop

--- src/angular/drag-and-drop/assets/scripts/app/app.js
+++ src/angular/drag-and-drop/assets/scripts/app/app.js
@@ -1,4 +1,29 @@
-  angular.module('app', ['ngDragDrop'])
-      .controller('appCtrl', ['$scope', function($scope) {
-          $scope.data = "drag me";
+  angular.module('app', ['ngDragDrop', 'ui.router'])
+      .run(
+          ['$rootScope', '$state', '$stateParams',
+              function($rootScope, $state, $stateParams) {
+                  $rootScope.$state = $state;
+                  $rootScope.$stateParams = $stateParams;
+              }
+          ]
+      )
+      .config(['$urlRouterProvider', '$stateProvider', '$locationProvider', '$compileProvider', 
+      	function($urlRouterProvider, $stateProvider, $locationProvider, $compileProvider) {
+
+          //if (!window.app.isLocalhost ) {
+          //    $compileProvider.debugInfoEnabled(false); // better perfomance
+          //}
+
+          $urlRouterProvider.otherwise('/');
+
+          // $locationProvider.html5Mode(true);  // mimic postback url even though it is a SPA
+
+          $stateProvider
+              .state('default', {
+                  url: '/',
+                  templateUrl: 'templates/start-page.html',
+                  controller: ['$scope', function($scope) {
+                      $scope.data = "drag me";
+                  }]
+              });
       }]);
