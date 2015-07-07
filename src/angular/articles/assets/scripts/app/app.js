a64f7701f03d1006c48edf19e911cef4176fba6a
Kunuk Nykjær <kunukn@gmail.com>
2015-07-07 13:48:03 +0200

    add isotope

--- src/angular/articles/assets/scripts/app/app.js
+++ src/angular/articles/assets/scripts/app/app.js
@@ -1,200 +1,234 @@
 ﻿;
 'use strict';
 
-(function setupAngular() {
 
-    var appVersion = 1;
+(function() {
 
-    var mainTemplates = {
-            header: 'templates/header-view.html',
-            footer: 'templates/footer-view.html'
-        },
-        frontpageArticleTemplates = {
-            articles: 'templates/frontpage/articles-page.html',
-            basic: 'templates/frontpage/article-basic-view.html',
-            popup: 'templates/frontpage/article-popup-view.html',
-            video: 'templates/frontpage/article-video-view.html'
-        },
-        articleDetailTemplates = {
-            bigImage: 'templates/article-detail/big-image-view.html',
-            smallImage: 'templates/article-detail/small-image-view.html',
-            carousel: 'templates/article-detail/carousel-view.html'
-        };
+    var $containerForIsotope;
 
-    var angularApp = angular
-        .module('app', ['ui.router'])
+    (function setupAngular() {
 
-    .run(
-            ['$rootScope', '$state', '$stateParams', '$templateCache', '$http',
-                function($rootScope, $state, $stateParams, $templateCache, $http) {
-                    $rootScope.$state = $state;
-                    $rootScope.$stateParams = $stateParams;
+        var appVersion = 1;
 
-                    $rootScope.$on('$stateChangeSuccess', function(ev, to, toParams, from, fromParams) {
-                        $rootScope.previousState = from.name;
-                        $rootScope.currentState = to.name;
-                    });
-
-                    try {
-                        // preload and cache template files
-
-                        // main
-                        $http.get(mainTemplates.header + '?v=' + appVersion, {
-                            cache: $templateCache
-                        });
-                        $http.get(mainTemplates.footer + '?v=' + appVersion, {
-                            cache: $templateCache
-                        });
-
-                        // frontpage
-                        $http.get(frontpageArticleTemplates.articles + '?v=' + appVersion, {
-                            cache: $templateCache
-                        });
-                        $http.get(frontpageArticleTemplates.basic + '?v=' + appVersion, {
-                            cache: $templateCache
-                        });
-                        $http.get(frontpageArticleTemplates.popup + '?v=' + appVersion, {
-                            cache: $templateCache
-                        });
-                        $http.get(frontpageArticleTemplates.video + '?v=' + appVersion, {
-                            cache: $templateCache
-                        });
-
-                        // article detail
-                        $http.get(articleDetailTemplates.bigImage + '?v=' + appVersion, {
-                            cache: $templateCache
-                        });
-                        $http.get(articleDetailTemplates.smallImage + '?v=' + appVersion, {
-                            cache: $templateCache
-                        });
-                        $http.get(articleDetailTemplates.carousel + '?v=' + appVersion, {
-                            cache: $templateCache
-                        });
-
-
-
-                    } catch (e) {
-                        console.log(e);
-                    }
-                }
-            ]
-        )
-        // Shared across controllers
-        .factory('cache', function() {
-            return {
-                topics: [{
-                    title: 'All',
-                    value: '*'
-                }, {
-                    title: 'Corporate',
-                    value: 'tag-corporate'
-                }, {
-                    title: 'Customers',
-                    value: 'tag-customers'
-                }, {
-                    title: 'Financials',
-                    value: 'tag-financials'
-                }, {
-                    title: 'Technology',
-                    value: 'tag-technology'
-                }, {
-                    title: 'NNIT Way',
-                    value: 'tag-nnit-way'
-                }],
-
-
-                frontpageArticles: [{
-                    type: 'basic',
-                    width: 'x1',
-                    title: 'article 1',
-                    description: 'description 1'
-                }, {
-                    type: 'popup',
-                    width: 'x2',
-                    title: 'article 2',
-                    description: 'description 2'
-                }, {
-                    type: 'video',
-                    width: 'x1',
-                    title: 'article 3',
-                    description: 'description 3'
-                }, {
-                    type: 'basic',
-                    width: 'x1',
-                    title: 'article 4',
-                    description: 'description 4'
-                }, {
-                    type: 'popup',
-                    width: 'x2',
-                    title: 'article 5',
-                    description: 'description 5'
-                }, {
-                    type: 'carousel',
-                    width: 'x2',
-                    title: 'article 6',
-                    description: 'description 6'
-                }],
-
-                frontpageTemplates: {
-                    basic: 'templates/frontpage-article-basic-view.html',
-                    popup: 'templates/frontpage-article-popup-view.html',
-                    video: 'templates/frontpage-article-video-view.html'
-                }
+        var mainTemplates = {
+                header: 'templates/header-view.html',
+                footer: 'templates/footer-view.html'
+            },
+            frontpageArticleTemplates = {
+                articles: 'templates/frontpage/articles-page.html',
+                basic: 'templates/frontpage/article-basic-view.html',
+                popup: 'templates/frontpage/article-popup-view.html',
+                video: 'templates/frontpage/article-video-view.html'
+            },
+            articleDetailTemplates = {
+                bigImage: 'templates/article-detail/big-image-view.html',
+                smallImage: 'templates/article-detail/small-image-view.html',
+                carousel: 'templates/article-detail/carousel-view.html'
             };
-        })
 
-    .config(['$urlRouterProvider', '$stateProvider', '$locationProvider', '$compileProvider',
-        function($urlRouterProvider, $stateProvider, $locationProvider, $compileProvider) {
+        var angularApp = angular
+            .module('app', ['ui.router'])
 
-            //if (!window.app.isLocalhost ) {
-            //    $compileProvider.debugInfoEnabled(false); // better perfomance
-            //}
+        .run(
+                ['$rootScope', '$state', '$stateParams', '$templateCache', '$http',
+                    function($rootScope, $state, $stateParams, $templateCache, $http) {
+                        $rootScope.$state = $state;
+                        $rootScope.$stateParams = $stateParams;
 
-            $urlRouterProvider.otherwise('/');
+                        $rootScope.$on('$stateChangeSuccess', function(ev, to, toParams, from, fromParams) {
+                            $rootScope.previousState = from.name;
+                            $rootScope.currentState = to.name;
+                        });
 
-            // $locationProvider.html5Mode(true);  // mimic postback url even though it is a SPA
+                        try {
+                            // preload and cache template files
 
-            $stateProvider
-                .state('home', {
-                    url: '/',
-                    templateUrl: frontpageArticleTemplates.articles,
-                    controller: ['$scope', 'cache', function($scope, cache) {
-                        $scope.frontpageArticles = cache.frontpageArticles || [];
-                        $scope.getFrontpageArticleTemplate = function(type) {
-                            switch (type) {
-                                case "basic":
-                                    return frontpageArticleTemplates.basic;
-                                case "popup":
-                                    return frontpageArticleTemplates.popup;
-                                case "video":
-                                    return frontpageArticleTemplates.video;
-                            }
-                            return frontpageArticleTemplates.basic;
-                        };
-                        $scope.info = function(message) {
-                            toastr.info(message);
+                            // main
+                            $http.get(mainTemplates.header + '?v=' + appVersion, {
+                                cache: $templateCache
+                            });
+                            $http.get(mainTemplates.footer + '?v=' + appVersion, {
+                                cache: $templateCache
+                            });
+
+                            // frontpage
+                            $http.get(frontpageArticleTemplates.articles + '?v=' + appVersion, {
+                                cache: $templateCache
+                            });
+                            $http.get(frontpageArticleTemplates.basic + '?v=' + appVersion, {
+                                cache: $templateCache
+                            });
+                            $http.get(frontpageArticleTemplates.popup + '?v=' + appVersion, {
+                                cache: $templateCache
+                            });
+                            $http.get(frontpageArticleTemplates.video + '?v=' + appVersion, {
+                                cache: $templateCache
+                            });
+
+                            // article detail
+                            $http.get(articleDetailTemplates.bigImage + '?v=' + appVersion, {
+                                cache: $templateCache
+                            });
+                            $http.get(articleDetailTemplates.smallImage + '?v=' + appVersion, {
+                                cache: $templateCache
+                            });
+                            $http.get(articleDetailTemplates.carousel + '?v=' + appVersion, {
+                                cache: $templateCache
+                            });
+
+
+
+                        } catch (e) {
+                            console.log(e);
                         }
-                    }]
-                });
-        }
-    ])
+                    }
+                ]
+            )
+            // Shared across controllers
+            .factory('cache', function() {
+                return {
+                    topics: [{
+                        title: 'All',
+                        value: '*'
+                    }, {
+                        title: 'Corporate',
+                        value: 'tag-corporate'
+                    }, {
+                        title: 'Customers',
+                        value: 'tag-customers'
+                    }, {
+                        title: 'Financials',
+                        value: 'tag-financials'
+                    }, {
+                        title: 'Technology',
+                        value: 'tag-technology'
+                    }, {
+                        title: 'NNIT Way',
+                        value: 'tag-nnit-way'
+                    }],
 
-    .controller('TopicsCtrl', function($scope, cache) {
-        $scope.topics = cache.topics;
-        $scope.info = function(message) {
-            toastr.info(message);
-        }
-    })
 
-    ;
+                    frontpageArticles: [{
+                        type: 'basic',
+                        width: 'x2',
+                        title: 'article 1',
+                        description: 'description 1',
+                        tags: ['tag-corporate', 'tag-nnit-way']
+                    }, {
+                        type: 'popup',
+                        width: 'x2',
+                        title: 'article 2',
+                        description: 'description 2',
+                        tags: ['tag-corporate', 'tag-technology'],
+                        detail: 'big-image'
+                    }, {
+                        type: 'video',
+                        width: 'x1',
+                        title: 'article 3',
+                        description: 'description 3',
+                        tags: ['tag-technology', 'tag-financials']
+                    }, {
+                        type: 'basic',
+                        width: 'x1',
+                        title: 'article 4',
+                        description: 'description 4',
+                        tags: ['tag-corporate', 'tag-financials']
+                    }, {
+                        type: 'popup',
+                        width: 'x2',
+                        title: 'article 5',
+                        description: 'description 5',
+                        tags: ['tag-corporate', 'tag-financials'],
+                        detail: 'small-image'
+                    }, {
+                        type: 'popup',
+                        width: 'x2',
+                        title: 'article 6',
+                        description: 'description 6',
+                        tags: ['tag-financials'],
+                        detail: 'carousel'
+                    }],
 
-    if (window.toastr) {
-        toastr.options.showMethod = 'slideDown';
-        toastr.options.progressBar = true;
-        toastr.options.positionClass = 'toast-bottom-right';
-        toastr.options.tapToDismiss = true;
-        toastr.options.fadeOut = 500;
-        toastr.options.timeOut = 1500;
-    }
+                    frontpageTemplates: {
+                        basic: 'templates/frontpage-article-basic-view.html',
+                        popup: 'templates/frontpage-article-popup-view.html',
+                        video: 'templates/frontpage-article-video-view.html'
+                    }
+                };
+            })
+
+        .config(['$urlRouterProvider', '$stateProvider', '$locationProvider', '$compileProvider',
+            function($urlRouterProvider, $stateProvider, $locationProvider, $compileProvider) {
+
+                //if (!window.app.isLocalhost ) {
+                //    $compileProvider.debugInfoEnabled(false); // better perfomance
+                //}
+
+                $urlRouterProvider.otherwise('/');
+
+                // $locationProvider.html5Mode(true);  // mimic postback url even though it is a SPA
+
+                $stateProvider
+                    .state('home', {
+                        url: '/',
+                        templateUrl: frontpageArticleTemplates.articles,
+                        controller: ['$scope', 'cache', function($scope, cache) {
+                            $scope.frontpageArticles = cache.frontpageArticles || [];
+                            $scope.getFrontpageArticleTemplate = function(type) {
+                                switch (type) {
+                                    case "basic":
+                                        return frontpageArticleTemplates.basic;
+                                    case "popup":
+                                        return frontpageArticleTemplates.popup;
+                                    case "video":
+                                        return frontpageArticleTemplates.video;
+                                }
+                                return frontpageArticleTemplates.basic;
+                            };
+                            $scope.info = function(message) {
+                                toastr.info(message);
+                            }
+                        }]
+                    });
+            }
+        ])
+
+        .controller('TopicsCtrl', function($scope, cache) {
+            $scope.topics = cache.topics;
+            $scope.info = function(message) {
+                toastr.info(message);
+            }
+        });
+    })();
+
+    (function domReady($) {
+
+        $(function() {
+
+            (function setupIsotope() {
+                setTimeout(function() {
+
+                    $containerForIsotope = $('.frontpage-articles').isotope({
+                        itemSelector: '.article',
+                        layoutMode: 'masonry',
+                        masonry: {
+                            columnWidth: 320
+                        }
+                    });
+                }, 2000); // wait until dom has been populated with data
+            })();
+
+            (function setupToastr() {
+                if (window.toastr) {
+                    toastr.options.showMethod = 'slideDown';
+                    toastr.options.progressBar = true;
+                    toastr.options.positionClass = 'toast-bottom-right';
+                    toastr.options.tapToDismiss = true;
+                    toastr.options.fadeOut = 500;
+                    toastr.options.timeOut = 1500;
+                }
+            })();
+        });
+    })(jQuery);
 
 })();
