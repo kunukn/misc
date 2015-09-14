4ad7c1bb4fe848ed4690f0ed1fe6559651945e29
Kunuk Nykjær <kunukn@gmail.com>
2015-09-14 22:42:30 +0200

    add postcss svg

--- src/postcss/example1/gulpfile.js
+++ src/postcss/example1/gulpfile.js
@@ -4,6 +4,12 @@ var gulp = require('gulp'),
     sass = require('gulp-sass'),
     browserSync = require('browser-sync').create();
 
+// --- postcss
+require('es6-promise').polyfill();
+var postcss = require('gulp-postcss'),
+    autoprefixer = require('autoprefixer-core'),
+    writesvg = require('postcss-write-svg');
+
 gulp.task('default', ['sass:watch'], function() {
     browserSync.init({
         server: {
@@ -13,10 +19,17 @@ gulp.task('default', ['sass:watch'], function() {
     });
 });
 
-
 gulp.task('sass', function() {
+    var processors = [
+        autoprefixer({
+            browsers: ['last 1 version']
+        }),
+        writesvg({ /* options */ })
+    ];
+
     gulp.src('./assets/styles/app/*.scss')
         .pipe(sass.sync().on('error', sass.logError))
+        .pipe(postcss(processors))
         .pipe(gulp.dest('./assets/styles/app'));
 });
 
@@ -25,18 +38,14 @@ gulp.task('sass:watch', function() {
 });
 
 
-// --- postcss
-require('es6-promise').polyfill();
-var postcss = require('gulp-postcss')
-var autoprefixer = require('autoprefixer-core');
-
-gulp.task('css', function() {
-    var processors = [
-        autoprefixer({
-            browsers: ['last 1 version']
-        })
-    ];
-    return gulp.src('./assets/styles/app/*.css')
-        .pipe(postcss(processors))
-        .pipe(gulp.dest('./assets/styles/postcss'));
-});
+// gulp.task('postcss', function() {
+//     var processors = [
+//         autoprefixer({
+//             browsers: ['last 1 version']
+//         }),
+//         writesvg({  options  })
+//     ];
+//     return gulp.src('./assets/styles/app/*.css')
+//         .pipe(postcss(processors))
+//         .pipe(gulp.dest('./assets/styles/postcss'));
+// });
