5face9b982ef0817a67bfc9159557a487a1eccbb
Kunuk Nykjær <kunukn@gmail.com>
2015-10-04 00:23:50 +0200

    custom error checker

--- src/pentia-housewarming/assets/scripts/app/app.js
+++ src/pentia-housewarming/assets/scripts/app/app.js
@@ -5,9 +5,9 @@
     var app =
         angular.module('app', [])
         .service('signupService', ['$http', function($http) {
-            var urlBase = 'http://example.com/signup';
-            this.newSignup = (model) => {
-                return $http.post(urlBase, model);
+            var url = 'http://example.com/signup';
+            this.newSignup = function(model) {
+                return $http.post(url, model);
             };
         }])
 
@@ -20,24 +20,18 @@
                 guests: 0
             };
 
-
-            /*
-            Start hovering zepperliner after other animation effect.
-            I could avoid this if I didn't use animate.css library and extracted the needed part, but that takes time...
-            */
-            $timeout(function() {
-                $scope.zeppelinerAnimation = {
-                    'animation': 'move-up-and-down 2.1s 2s infinite'
-                };
-            }, 3000);
-
-            $scope.signup = () => {
+            $scope.signup = function() {
                 console.log('signup clicked');
                 console.log($scope.signupModel);
                 // signupService.newSignup($scope.signupModel);
                 alert(JSON.stringify($scope.signupModel));
                 return false;
             };
+
+            $scope.emailValidate = function(email) {
+                // Naive custom email checker
+                return email && email.indexOf('@') > -1 && email.indexOf('.') > -1;
+            }
         }]);
 
 })();
