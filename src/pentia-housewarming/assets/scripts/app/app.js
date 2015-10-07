bf0972c713255f6f6bee7460127c7214771d9f70
Kunuk Nykjær <kunukn@gmail.com>
2015-10-07 21:35:49 +0200

    no js support

--- src/pentia-housewarming/assets/scripts/app/app.js
+++ src/pentia-housewarming/assets/scripts/app/app.js
@@ -29,8 +29,8 @@
             };
 
             $scope.emailValidate = function(email) {
-                // Naive custom email checker
-                return email && email.indexOf('@') > -1 && email.indexOf('.') > -1;
+                var emailRegex = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+                return emailRegex.test(email);
             }
         }]);
 
