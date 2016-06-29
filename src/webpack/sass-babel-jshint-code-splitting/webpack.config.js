11cc30b0fd859c1f155b6e269a002796683c5730
Kunuk Nykjær <kunukn@gmail.com>
2016-06-29 08:37:35 +0200

    format

--- src/webpack/sass-babel-jshint-code-splitting/webpack.config.js
+++ src/webpack/sass-babel-jshint-code-splitting/webpack.config.js
@@ -4,26 +4,26 @@ var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 
 module.exports = {
-	context: path.resolve('assets'),
-	entry: {
-		app: ['./scripts/app'],
-		vendors: ['jquery', 'knockout']
-	},
-	output: {
-		path: path.resolve('public/assets/'),
-		publicPath: '/public/assets/',
-		filename: '[name].bundle.js',
+    context: path.resolve('assets'),
+    entry: {
+        app: ['./scripts/app'],
+        vendors: ['jquery', 'knockout']
+    },
+    output: {
+        path: path.resolve('public/assets/'),
+        publicPath: '/public/assets/',
+        filename: '[name].bundle.js',
         libraryTarget: 'var',
         library: 'App'
-	},
-		devServer: {
-		contentBase: './public',
-			noInfo: true, //  --no-info option
-			//hot: true,
-			//inline: true
-		},
-	 plugins: [
-	   new ExtractTextPlugin('app.css'),
+    },
+    devServer: {
+        contentBase: './public',
+        noInfo: true, //  --no-info option
+        //hot: true,
+        //inline: true
+    },
+    plugins: [
+        new ExtractTextPlugin('app.css'),
         new webpack.ProvidePlugin({
             $: "jquery",
             jquery: "jQuery",
@@ -31,30 +31,30 @@ module.exports = {
         }),
         new webpack.optimize.CommonsChunkPlugin('vendors', 'vendors.bundle.js', Infinity)
     ],
-	module: {
-		preLoaders: [
-		{
-				test: /\.js$/,
-				exclude: /node_modules/,
-				loader: 'jshint-loader'
-			}],
-		loaders: [
-
-		{ test: require.resolve('jquery'), loader: 'expose?jQuery!expose?$' },
-			{
-				test: /\.es6$/,
-				exclude: /node_modules/,
-				loader: 'babel-loader'
-			},
-
-			{
-				test: /\.scss$/,
-				exclude: /node_modules/,
-				loader: ExtractTextPlugin.extract('style-loader','css-loader!autoprefixer-loader!sass-loader')
-			}
-		]
-	},
-	resolve:{
-		extensions: ['', '.js', '.es6', '.ts']
-	}
+    module: {
+        preLoaders: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: 'jshint-loader'
+        }],
+        loaders: [
+            {
+                test: require.resolve('jquery'),
+                exclude: /node_modules/,
+                loader: 'expose?jQuery!expose?$'
+            }, {
+                test: /\.es6$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.scss$/,
+                exclude: /node_modules/,
+                loader: ExtractTextPlugin.extract('style-loader', 'css-loader!autoprefixer-loader!sass-loader')
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['', '.js', '.es6', '.ts']
+    }
 }
