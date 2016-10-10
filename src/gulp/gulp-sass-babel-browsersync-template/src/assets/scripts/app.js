c0645167bdc1432920b545aac417eb86eac0601c
Kunuk Nykjær <kunukn@gmail.com>
2016-10-10 15:25:10 +0200

    prep unit test

--- src/gulp/gulp-sass-babel-browsersync-template/src/assets/scripts/app.js
+++ src/gulp/gulp-sass-babel-browsersync-template/src/assets/scripts/app.js
@@ -5,4 +5,3 @@ import time from "./time.js";
 document.getElementById('time').innerHTML = time();
 
 export default 'app';
-
---
+++ src/gulp/gulp-sass-babel-browsersync-template/src/assets/scripts/time.spec.js
@@ -0,0 +1,10 @@
+'use strict';
+
+//import time from './time.js';
+//var time = require('./time.js');
+
+describe('time', function() {
+    it('should return something', function() {
+        expect(true).toBe(true);
+    });
+});
