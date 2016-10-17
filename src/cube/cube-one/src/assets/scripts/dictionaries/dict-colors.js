0d9d247386fabe37980dcebc75ac4fab63066f50
Kunuk Nykjær <kunukn@gmail.com>
2016-10-17 21:43:56 +0200

    add img

--- src/cube/cube-one/src/assets/scripts/dictionaries/dict-colors.js
+++ src/cube/cube-one/src/assets/scripts/dictionaries/dict-colors.js
@@ -7,6 +7,14 @@ dictColors['r'] = 'linear-gradient(135deg, #C41E3A, white)';
 dictColors['l'] = 'linear-gradient(135deg, #FF5800, white)';
 dictColors['b'] = 'linear-gradient(135deg, #0051BA, white)';
 dictColors['d'] = 'linear-gradient(135deg, #FFD500, white)';
+
+dictColors['u'] = '#fff url("/src/assets/images/lion.svg") no-repeat center center / contain';
+dictColors['f'] = '#009E60 url("/src/assets/images/lion.svg") no-repeat center center / contain';
+dictColors['r'] = '#C41E3A url("/src/assets/images/lion.svg") no-repeat center center / contain';
+dictColors['l'] = '#FF5800 url("/src/assets/images/lion.svg") no-repeat center center / contain';
+dictColors['b'] = '#0051BA url("/src/assets/images/lion.svg") no-repeat center center / contain';
+dictColors['d'] = '#FFD500 url("/src/assets/images/lion.svg") no-repeat center center / contain';
+
 deepFreeze(dictColors);
 
 export default dictColors;
