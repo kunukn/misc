c75bd80146eddf8d046cf45f95e097808c844b3e
Kunuk Nykjær <kunukn@gmail.com>
2016-10-17 22:46:30 +0200

    add transforms

--- src/cube/cube-one/src/assets/scripts/dictionaries/dict-cube-transform.js
+++ src/cube/cube-one/src/assets/scripts/dictionaries/dict-cube-transform.js
@@ -7,9 +7,13 @@ Object.keys(STATES).forEach(i => dictCubeTransform[i] = []);
 
 dictCubeTransform['lf']['u'] = { angle: 90, dir: 'Z' };
 dictCubeTransform['rf']['u'] = { angle: -90, dir: 'Z' };
+dictCubeTransform['fl']['u'] = { angle: -90, dir: 'Z' };
+dictCubeTransform['fu']['u'] = { angle: 180, dir: 'Z' };
+dictCubeTransform['fr']['u'] = { angle: 90, dir: 'Z' };
 
 dictCubeTransform['lf']['f'] = { angle: 90, dir: 'Z' };
 dictCubeTransform['rf']['f'] = { angle: -90, dir: 'Z' };
+dictCubeTransform['db']['f'] = { angle: 180, dir: 'Z' };
 
 dictCubeTransform['fd']['r'] = { angle: 90, dir: 'Z' };
 dictCubeTransform['db']['r'] = { angle: 180, dir: 'Z' };
@@ -33,6 +37,8 @@ dictCubeTransform['bu']['u'] = { angle: 180, dir: 'Z' };
 
 dictCubeTransform['fd']['b'] = { angle: 180, dir: 'Z' };
 dictCubeTransform['dr']['b'] = { angle: 180, dir: 'Z' };
+dictCubeTransform['db']['b'] = { angle: 180, dir: 'Z' };
+
 
 dictCubeTransform['lf']['d'] = { angle: 90, dir: 'Z' };
 
