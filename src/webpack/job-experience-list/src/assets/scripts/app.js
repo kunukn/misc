ea79b6d4bc4ed4f69cbaeed9758d8bf4884f83b3
Kunuk Nykjær <kunukn@gmail.com>
2016-11-15 10:03:08 +0100

    upd

--- src/webpack/job-experience-list/src/assets/scripts/app.js
+++ src/webpack/job-experience-list/src/assets/scripts/app.js
@@ -2,43 +2,22 @@
 'use strict';
 import styles from '../styles/app.scss'; // Apply styling
 
-console.log('App running');
-
-var log = console.log.bind(console);
+const log = console.log.bind(console);
 
 
 function qs(expr, context) {
-  return (context || document).querySelector(expr);
+    return (context || document).querySelector(expr);
 }
 
 function qsa(expr, context) {
-  return [].slice.call((context || document).querySelectorAll(expr), 0);
+    return [].slice.call((context || document).querySelectorAll(expr), 0);
 }
 
-function byId(id){
-  return document.getElementById(id);
+function byId(id) {
+    return document.getElementById(id);
 }
 
-
-qsa('.js-work-history__toggle').forEach(function(toggle){
-    //toggle.
-});
-
-// $('.js-work-history__toggle').each(function(index, element) {
-//     var $button = $(element),
-//         $ships = $button.closest('.js-work-history'),
-//         $collapsible = $ships.find('.js-work-history__content').first();
-
-//     // init
-//     $collapsible.slideToggle(0);
-//     $button.toggleClass('sfs-show');
-
-//     // event binding
-//     $button.click(function(event) {
-//         $collapsible.slideToggle(200);
-//         $button.toggleClass('show');
-//     });
-// });
+log('App running');
 
 export class App {
 
