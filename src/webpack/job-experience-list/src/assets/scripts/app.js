e99c4a1374c994deb67bc822e0024d145c57fed3
Kunuk Nykjær <kunukn@gmail.com>
2016-11-16 07:54:30 +0100

    use class

--- src/webpack/job-experience-list/src/assets/scripts/app.js
+++ src/webpack/job-experience-list/src/assets/scripts/app.js
@@ -4,40 +4,44 @@ import styles from '../styles/app.scss'; // Apply styling
 
 const log = console.log.bind(console);
 
-function Job(data) {
-    var self = this;
-    self.company = data.company;
-    self.durationTotal = data.durationTotal;
-    self.title = ko.observable('');
-}
-
-function Role(data) {
-    var self = this;
-
-
-    self.title = data.title;
-    self.durationTotal = data.durationTotal;
-    self.durationFiveYears = data.durationFiveYears;
-    self.jobs = ko.observableArray([]);
-
-    if (data.jobs) {
-        data.jobs.forEach(function(item) {
-            self.jobs.push(new Job(item));
-        });
+class Job {
+    constructor(data) {
+        var self = this;
+        self.company = data.company;
+        self.durationTotal = data.durationTotal;
+        self.title = ko.observable('');
     }
 }
 
-function ViewModel(data) {
-    var self = this;
-
-    self.isDebug = ko.observable(data.isDebug),
+class Role {
+    constructor(data) {
+        var self = this;
         self.title = data.title;
-    self.roles = ko.observableArray([]);
+        self.durationTotal = data.durationTotal;
+        self.durationFiveYears = data.durationFiveYears;
+        self.jobs = ko.observableArray([]);
 
-    if (data.roles) {
-        data.roles.forEach(function(item) {
-            self.roles.push(new Role(item));
-        });
+        if (data.jobs) {
+            data.jobs.forEach(function(item) {
+                self.jobs.push(new Job(item));
+            });
+        }
+    }
+}
+
+class ViewModel {
+    constructor(data) {
+        var self = this;
+
+        self.isDebug = ko.observable(data.isDebug),
+            self.title = data.title;
+        self.roles = ko.observableArray([]);
+
+        if (data.roles) {
+            data.roles.forEach(function(item) {
+                self.roles.push(new Role(item));
+            });
+        }
     }
 }
 
@@ -45,35 +49,35 @@ function ViewModel(data) {
 var viewModel = new ViewModel({
     isDebug: false,
     title: 'Positions',
-    roles: [
-    {
-        title: 'Architect',
-        durationTotal: '5 years 1 month',
-        durationFiveYears: '2 years 1 month',
-        jobs: [{
-                company: 'ABC',
-                durationTotal: '2 years 1 month'
-            },
-            {
-                company: 'DBC',
-                durationTotal: '1 years 1 month'
-            }
-        ]
-    },
-    {
-        title: 'Developer',
-        durationTotal: '5 years 1 month',
-        durationFiveYears: '2 years 1 month',
-        jobs: [{
-                company: 'ABC',
-                durationTotal: '2 years 1 month'
-            },
-            {
-                company: 'DBC',
-                durationTotal: '1 years 1 month'
-            }
-        ]
-    }],
+    roles: [{
+            title: 'Architect',
+            durationTotal: '5 years 1 month',
+            durationFiveYears: '2 years 1 month',
+            jobs: [{
+                    company: 'ABC',
+                    durationTotal: '2 years 1 month'
+                },
+                {
+                    company: 'DBC',
+                    durationTotal: '1 years 1 month'
+                }
+            ]
+        },
+        {
+            title: 'Developer',
+            durationTotal: '5 years 1 month',
+            durationFiveYears: '2 years 1 month',
+            jobs: [{
+                    company: 'ABC',
+                    durationTotal: '2 years 1 month'
+                },
+                {
+                    company: 'DBC',
+                    durationTotal: '1 years 1 month'
+                }
+            ]
+        }
+    ],
 });
 ko.applyBindings(viewModel, document.getElementById('app'));
 window.vm = window.viewModel = viewModel;
