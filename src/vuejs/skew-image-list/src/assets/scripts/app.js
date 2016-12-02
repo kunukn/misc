1ccd15df3c4003832b5ea555daf7aa740536121c
Kunuk Nykjær <kunukn@gmail.com>
2016-12-02 09:56:50 +0100

    upd

--- src/vuejs/skew-image-list/src/assets/scripts/app.js
+++ src/vuejs/skew-image-list/src/assets/scripts/app.js
@@ -1,43 +1,50 @@
 'use strict';
 
 import styles from '../styles/app.scss'; // Apply styling
-import Vue from 'vue';
+import vue from 'vue';
 
 const log = console.log.bind(console);
+
+log(vue);
+
+
+
 
 log('App running');
 
 const example1 = new Vue({
-  el: '#skew-images',
+  el: '#skewImages',
   data: {
     isDebug: false,
     isSkewReversed: false,
-    items: [
-      { backgroundImage: 'url("http://fillmurray.com/200/200")', backgroundColor: 'transparent'},
-      { backgroundImage: 'url("http://fillmurray.com/210/210")', backgroundColor: 'transparent'},
+    backgroundColor: 'transparent',
+    borderColor: 'gray',
+    items: [       
+       { backgroundImage: 'url("http://fillmurray.com/200/200")', backgroundColor: 'transparent'},
+       { backgroundImage: 'url("http://fillmurray.com/210/210")', backgroundColor: 'transparent'},
+       { backgroundImage: 'url("http://fillmurray.com/220/220")', backgroundColor: 'transparent'},
+       { backgroundImage: 'url("http://fillmurray.com/230/230")', backgroundColor: 'transparent'},
+       { backgroundImage: 'url("http://fillmurray.com/240/240")', backgroundColor: 'transparent'},
+       { backgroundImage: 'url("http://fillmurray.com/250/250")', backgroundColor: 'transparent'},
+       { backgroundImage: 'url("http://fillmurray.com/200/200")', backgroundColor: 'transparent'},
+       { backgroundImage: 'url("http://fillmurray.com/210/210")', backgroundColor: 'transparent'},
       { backgroundImage: 'url("http://fillmurray.com/220/220")', backgroundColor: 'transparent'},
       { backgroundImage: 'url("http://fillmurray.com/230/230")', backgroundColor: 'transparent'},
       { backgroundImage: 'url("http://fillmurray.com/240/240")', backgroundColor: 'transparent'},
       { backgroundImage: 'url("http://fillmurray.com/250/250")', backgroundColor: 'transparent'},
-      { backgroundImage: 'url("http://fillmurray.com/200/200")', backgroundColor: 'transparent'},
-      { backgroundImage: 'url("http://fillmurray.com/210/210")', backgroundColor: 'transparent'},
-      { backgroundImage: 'url("http://fillmurray.com/220/220")', backgroundColor: 'transparent'},
-      { backgroundImage: 'url("http://fillmurray.com/230/230")', backgroundColor: 'transparent'},
-      { backgroundImage: 'url("http://fillmurray.com/240/240")', backgroundColor: 'transparent'},
-      { backgroundImage: 'url("http://fillmurray.com/250/250")', backgroundColor: 'transparent'},
-      { backgroundImage: 'url("http://fillmurray.com/200/200")', backgroundColor: 'transparent'},
-      { backgroundImage: 'url("http://fillmurray.com/210/210")', backgroundColor: 'transparent'},
-      { backgroundImage: 'url("http://fillmurray.com/220/220")', backgroundColor: 'transparent'},
-      { backgroundImage: 'url("http://fillmurray.com/230/230")', backgroundColor: 'transparent'},
-      { backgroundImage: 'url("http://fillmurray.com/240/240")', backgroundColor: 'transparent'},
-      { backgroundImage: 'url("http://fillmurray.com/250/250")', backgroundColor: 'transparent'},
-      { backgroundImage: 'url("http://fillmurray.com/200/200")', backgroundColor: 'transparent'},
-      { backgroundImage: 'url("http://fillmurray.com/210/210")', backgroundColor: 'transparent'},
-      { backgroundImage: 'url("http://fillmurray.com/220/220")', backgroundColor: 'transparent'},
-      { backgroundImage: 'url("http://fillmurray.com/230/230")', backgroundColor: 'transparent'},
-      { backgroundImage: 'url("http://fillmurray.com/240/240")', backgroundColor: 'transparent'},
-      { backgroundImage: 'url("http://fillmurray.com/250/250")', backgroundColor: 'transparent'},         
-      { backgroundImage: 'url("http://fillmurray.com/200/200")', backgroundColor: 'transparent'},
+      // { backgroundImage: 'url("http://fillmurray.com/200/200")', backgroundColor: 'transparent'},
+      // { backgroundImage: 'url("http://fillmurray.com/210/210")', backgroundColor: 'transparent'},
+      // { backgroundImage: 'url("http://fillmurray.com/220/220")', backgroundColor: 'transparent'},
+      // { backgroundImage: 'url("http://fillmurray.com/230/230")', backgroundColor: 'transparent'},
+      // { backgroundImage: 'url("http://fillmurray.com/240/240")', backgroundColor: 'transparent'},
+      // { backgroundImage: 'url("http://fillmurray.com/250/250")', backgroundColor: 'transparent'},
+      // { backgroundImage: 'url("http://fillmurray.com/200/200")', backgroundColor: 'transparent'},
+      // { backgroundImage: 'url("http://fillmurray.com/210/210")', backgroundColor: 'transparent'},
+      // { backgroundImage: 'url("http://fillmurray.com/220/220")', backgroundColor: 'transparent'},
+      // { backgroundImage: 'url("http://fillmurray.com/230/230")', backgroundColor: 'transparent'},
+      // { backgroundImage: 'url("http://fillmurray.com/240/240")', backgroundColor: 'transparent'},
+      // { backgroundImage: 'url("http://fillmurray.com/250/250")', backgroundColor: 'transparent'},         
+      // { backgroundImage: 'url("http://fillmurray.com/200/200")', backgroundColor: 'transparent'},
     //   { backgroundImage: 'url("http://fillmurray.com/210/210")', backgroundColor: 'transparent'},
     //   { backgroundImage: 'url("http://fillmurray.com/220/220")', backgroundColor: 'transparent'},
     //   { backgroundImage: 'url("http://fillmurray.com/230/230")', backgroundColor: 'transparent'},
