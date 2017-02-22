47af8d867018f9cd069e7ba52fac80a7ab839944
Kunuk Nykjær <kunukn@gmail.com>
2017-02-22 20:58:26 +0100

    upd

--- src/webpack2/postcss-custom-plugin/postcss-disable-property-value.js
+++ src/webpack2/postcss-custom-plugin/postcss-disable-property-value.js
@@ -3,23 +3,25 @@
 var postcss = require('postcss');
 
 module.exports = postcss.plugin('postcss-disable-property-value', function (options) {
-  var options = options || {}
+  var options = options || {};
 
   return function (root, result) {
 
-    if (Object.keys(options).length) {
+    //result.warn(      JSON.stringify(options));
+    var props = Object.keys(options);
+    props.map(function (prop, index) {
+      root.walkDecls(prop, function (decl) {
+          var value = options[prop];
+           result.warn( prop);
+           result.warn( value + '');
+           result.warn( decl.value);
 
-      // Runs through all of the nodes (declorations) in the file
-      root.walkDecls(declaration => {
-
-        if (options[declaration.prop]) {
-          var val = options[declaration.prop];
-          if (val === declaration.value) {
-            declaration.value = 'postcss-disable-' + val;
+          if (value === decl.value) {
+            //decl.remove();
+            decl.prop = prop + '__postcss-disable';
           }
-        }
-      });
-    }
+        })
+    });
   };
 });
 
--- src/webpack2/postcss-custom-plugin/postcss.config.js
+++ src/webpack2/postcss-custom-plugin/postcss.config.js
@@ -2,14 +2,12 @@
 
 module.exports = {
   plugins: [
-    //require('postcss-smart-import')({ /* ...options */ }),
-    //require('precss')({ /* ...options */ }),
-    //require('autoprefixer')({ /* ...options */ }),
     require('./postcss-disable-property-value.js')(
       { 
       'border-radius':'50%', 
       'background-color':'red',
       'display':'flex',
+      'content': "''", 
     }),  
   ]
 }
