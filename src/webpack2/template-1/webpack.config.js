f310bc86257f0b687f0352f8f73985ca58e55407
Kunuk Nykjær <kunukn@gmail.com>
2017-08-31 23:45:44 +0200

    modified import redirect

--- src/webpack2/template-1/webpack.config.js
+++ src/webpack2/template-1/webpack.config.js
@@ -1,69 +1,85 @@
 var path = require('path'),
-    webpack = require('webpack'),
-    ExtractTextPlugin = require('extract-text-webpack-plugin'),
-    HtmlWebpackPlugin = require('html-webpack-plugin');
+  webpack = require('webpack'),
+  ExtractTextPlugin = require('extract-text-webpack-plugin'),
+  HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = (env = {}) => {
+  var nodeEnv = env.production === true ? 'production' : 'development';
 
-    var nodeEnv = env.production === true
-        ? 'production'
-        : 'development';
+  console.log(`------------ ${nodeEnv} ------------`);
 
-    console.log(`------------ ${nodeEnv} ------------`);
+  var plugins = [
+    new ExtractTextPlugin('[name].css'),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify(nodeEnv)
+      }
+    }),
+    new HtmlWebpackPlugin({ template: 'src/index.html' }),
+    new webpack.NormalModuleReplacementPlugin(
+      /index\.scss/,
+      './index2.scss'
+    ),
+    new webpack.NormalModuleReplacementPlugin(
+      /components\/App\.jsx/,
+      './App2.jsx'
+    )
+  ];
 
-    var plugins = [
-        new ExtractTextPlugin('[name].css'), new webpack.DefinePlugin({
-            'process.env': {
-                NODE_ENV: JSON.stringify(nodeEnv)
-            }
-        }),
-        new HtmlWebpackPlugin({template: 'src/index.html'})
-    ];
-
-    return {
-        devtool: 'source-map',
-        context: path.resolve('./'),
-        entry: {
-            app: ['./src/index']
+  return {
+    devtool: 'source-map',
+    context: path.resolve('./'),
+    entry: {
+      app: ['./src/index']
+    },
+    output: {
+      path: path.resolve('dist'),
+      publicPath: '',
+      filename: '[name].bundle.js'
+    },
+    devServer: {
+      openPage: 'index.html',
+      open: true, // auto open browser?
+      contentBase: './',
+      noInfo: true,
+      port: 3456
+    },
+    plugins: plugins,
+    module: {
+      rules: [
+        {
+          test: /\.html$/,
+          exclude: [/node_modules/],
+          loader: 'html-loader'
         },
-        output: {
-            path: path.resolve('dist'),
-            publicPath: '',
-            filename: '[name].bundle.js'
+        {
+          test: /\.(js|jsx)$/,
+          exclude: [/node_modules/],
+          loaders: ['babel-loader']
         },
-        devServer: {
-            openPage: 'index.html',
-            open: true, // auto open browser?
-            contentBase: './',
-            noInfo: true,
-            port: 3456,
+        {
+          test: /\.scss$/,
+          exclude: [/node_modules/],
+          loader: ExtractTextPlugin.extract({
+            fallback: 'style-loader',
+            use: 'css-loader!sass-loader'
+          })
         },
-        plugins: plugins,
-        module: {
-            rules: [
-                {
-                    test: /\.html$/,
-                    exclude: [/node_modules/],
-                    loader: "html-loader"
-                }, {
-                    test: /\.(js|jsx)$/,
-                    exclude: [/node_modules/],
-                    loaders: ['babel-loader']
-                }, {
-                    test: /\.scss$/,
-                    exclude: [/node_modules/],
-                    loader: ExtractTextPlugin.extract({fallback: 'style-loader', use: 'css-loader!sass-loader'})
-                }, {
-                    test: /\.css$/,
-                    exclude: [/node_modules/],
-                    loader: ExtractTextPlugin.extract({fallback: 'style-loader', use: 'css-loader'})
-                }
-            ]
-        },
-        resolve: {
-            extensions: ['.js', '.jsx'],
-            modules: [path.resolve(__dirname, "src/components"), "node_modules"],
-        },
-        externals: {}
-    }
+        {
+          test: /\.css$/,
+          exclude: [/node_modules/],
+          loader: ExtractTextPlugin.extract({
+            fallback: 'style-loader',
+            use: 'css-loader'
+          })
+        }
+      ]
+    },
+    resolve: {
+      extensions: ['.js', '.jsx'],
+      modules: [path.resolve(__dirname, 'src/components'), 'node_modules'],
+      moduleExtensions: ['-loader']
+    },
+    externals: {}
+  };
 };
