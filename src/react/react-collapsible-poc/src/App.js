14d8b5a19b80432a27d481d5737247224e0a9193
Kunuk Nykjær <kunukn@gmail.com>
2017-12-08 19:34:33 +0100

    updates

--- src/react/react-collapsible-poc/src/App.js
+++ src/react/react-collapsible-poc/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-//import logo from './logo.svg';
-//import './App.css';
+import eases from 'eases';
 
+// https://github.com/kunukn/react-number-easing/blob/master/src/components/NumberEasing.jsx
 const log = console.log.bind(console);
 
 const TOGGLE = {
@@ -12,31 +12,49 @@ const TOGGLE = {
 };
 
 class ToggleBox extends Component {
-  state = {
-    box: TOGGLE.EXPANDED
+  static defaultProps = {
+    speed: 500,
+    ease: 'quintInOut',
   };
+
+  state = {
+    box: TOGGLE.EXPANDED,
+  };
+
   onToggle = () => {
     log(this.BoxElement.clientHeight);
-  }
+  };
   render() {
+
+    log(eases[this.props.ease](.3));
+
     //return this.props.children({onToggle: this.onToggle});
     return (
       <div className="togglebox">
-        <Toggle onToggle={this.onToggle}>toggle</Toggle>  
-        <Box inputRef={el => this.BoxElement = el}>box</Box>
+        <Toggle onToggle={this.onToggle}>toggle</Toggle>
+        <Box inputRef={el => (this.BoxElement = el)}>box</Box>
       </div>
     );
   }
+
+  componentWillReceiveProps(nextProps) {
+  }
+  shouldComponentUpdate(nextProps, nextState) {
+    return true;
+  }
+  componentWillUnmount() {
+    clearTimeout(this.timeout);
+    clearTimeout(this.delayTimeout);
+  }
+
 }
 
 class Toggle extends Component {
   render() {
-    return (      
+    return (
       <div className="togglebox__toggle">
-        <button onClick={this.props.onToggle}>
-          {this.props.children}
-        </button>        
-    </div>    
+        <button onClick={this.props.onToggle}>{this.props.children}</button>
+      </div>
     );
   }
 }
@@ -51,12 +69,9 @@ class Box extends Component {
   }
 }
 
-
 class App extends Component {
   render() {
-    return (
-      <ToggleBox/>
-    );
+    return <ToggleBox />;
   }
 }
 
