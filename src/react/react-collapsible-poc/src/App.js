5387674d8a9cb1badda9d5a0e423038b88c4abd8
Kunuk Nykjær <kunukn@gmail.com>
2017-12-10 20:01:05 +0100

    render callback

--- src/react/react-collapsible-poc/src/App.js
+++ src/react/react-collapsible-poc/src/App.js
@@ -5,40 +5,49 @@ import eases from 'eases';
 const log = console.log.bind(console);
 
 const TOGGLE = {
-  EXPANDED: 1,
-  COLLAPSED: 2,
-  EXPANDING: 3,
-  COLLAPSING: 4,
+  EXPANDED: 'EXPANDED',
+  COLLAPSED: 'COLLAPSED',
+  EXPANDING: 'EXPANDING',
+  COLLAPSING: 'COLLAPSING',
 };
 
 class ToggleBox extends Component {
   static defaultProps = {
     speed: 500,
     ease: 'quintInOut',
-  };
-
-  state = {
     box: TOGGLE.EXPANDED,
   };
 
-  onToggle = () => {
-    log(this.BoxElement.clientHeight);
+  state = {
+    box: this.props.box,
   };
+
+  setBoxElement = element => {
+    const el = element || null;
+    this.boxElement = el;
+    const boxHeight = (el && el.clientHeight) || 0;
+    this.setState({
+      boxHeight
+    });
+  };
+
+  onToggle = () => {
+    log(this.boxElement && this.boxElement.clientHeight);
+    log(eases[this.props.ease](0.3));
+  };
+
   render() {
-
-    log(eases[this.props.ease](.3));
-
-    //return this.props.children({onToggle: this.onToggle});
-    return (
-      <div className="togglebox">
-        <Toggle onToggle={this.onToggle}>toggle</Toggle>
-        <Box inputRef={el => (this.BoxElement = el)}>box</Box>
-      </div>
-    );
+    
+    return this.props.children({
+      onToggle: this.onToggle,
+      setBoxElement: this.setBoxElement,
+      state: this.state,
+    });
   }
 
-  componentWillReceiveProps(nextProps) {
+  componentDidUpdate(){
   }
+  componentWillReceiveProps(nextProps) {}
   shouldComponentUpdate(nextProps, nextState) {
     return true;
   }
@@ -46,32 +55,29 @@ class ToggleBox extends Component {
     clearTimeout(this.timeout);
     clearTimeout(this.delayTimeout);
   }
-
-}
-
-class Toggle extends Component {
-  render() {
-    return (
-      <div className="togglebox__toggle">
-        <button onClick={this.props.onToggle}>{this.props.children}</button>
-      </div>
-    );
-  }
-}
-
-class Box extends Component {
-  render() {
-    return (
-      <div className="togglebox__box" ref={this.props.inputRef}>
-        {this.props.children}
-      </div>
-    );
-  }
 }
 
 class App extends Component {
   render() {
-    return <ToggleBox />;
+    return (
+      <ToggleBox>
+        {({ onToggle, setBoxElement, state }) => {
+          return (
+            <div className="togglebox">
+              <div className="togglebox__toggle">
+                <button onClick={onToggle}>toggle</button>
+              </div>
+              <div className="togglebox__box" ref={setBoxElement}>
+                box
+              </div>
+              <pre>{
+                JSON.stringify(state, null, 2)
+              }</pre>
+            </div>
+          );
+        }}
+      </ToggleBox>
+    );
   }
 }
 
