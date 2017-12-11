f9702ad692b9b8cbcfeed563f9159997589f6638
Kunuk Nykjær <kny@tdc.dk>
2017-12-11 11:37:02 +0100

    updates

--- src/react/react-collapsible-poc/src/App.js
+++ src/react/react-collapsible-poc/src/App.js
@@ -1,8 +1,10 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react'; // eslint-disable-line
+import PropTypes from 'prop-types'; // eslint-disable-line
 import eases from 'eases';
 
 // https://github.com/kunukn/react-number-easing/blob/master/src/components/NumberEasing.jsx
 const log = console.log.bind(console);
+log(Object.keys(eases));
 
 const TOGGLE = {
   EXPANDED: 'EXPANDED',
@@ -12,32 +14,102 @@ const TOGGLE = {
 };
 
 class ToggleBox extends Component {
+
+  static propTypes = {
+    duration: PropTypes.number,
+    ease: PropTypes.oneOf(Object.keys(eases)),
+  };
+
   static defaultProps = {
-    speed: 500,
-    ease: 'quintInOut',
-    box: TOGGLE.EXPANDED,
+    duration: 500,
+    ease: 'linear',
+    //boxState: TOGGLE.EXPANDED,
   };
 
   state = {
-    box: this.props.box,
+    isAnimating: false,
+    //boxState: TOGGLE.EXPANDED,
   };
 
   setBoxElement = element => {
-    const el = element || null;
-    this.boxElement = el;
-    const boxHeight = (el && el.clientHeight) || 0;
-    this.setState({
-      boxHeight
-    });
+    this.boxElement = element || null;
   };
 
   onToggle = () => {
+    if (this.isAnimating) {
+      log('isAnimating true');
+      return;
+    }
+
+    if (this.state.boxState === TOGGLE.EXPANDED) {
+      this.boxState = TOGGLE.COLLAPSING;
+      this.isAnimating = true;
+    } else if (this.state.boxState === TOGGLE.COLLAPSED) {
+      this.boxState = TOGGLE.EXPANDING;
+      this.isAnimating = true;
+    } else {
+      log('error');
+    }
+
+    log(this.boxState);
+
     log(this.boxElement && this.boxElement.clientHeight);
     log(eases[this.props.ease](0.3));
+
+    this.updateHeight();
   };
 
+  updateHeight = () => {
+    //const duration = parseInt(this.props.duration, 10);
+
+    if (!this.boxElement) {
+      log('no boxElement');
+      return;
+    }
+
+    // const now = new Date().getTime();
+    // const elapsedTime = Math.min(
+    //   this.props.duration,
+    //   now - this.startAnimationTime,
+    // );
+    // const progress = eases[this.props.ease](elapsedTime / this.props.speed);
+
+    // const currentDisplayValue = Math
+    //   .round(((value - this.state.previousValue) * progress) + this.state.previousValue);
+
+    // this.setState({
+    //   displayValue: currentDisplayValue,
+    // });
+
+    // if (elapsedTime < this.props.duration) {
+    //   this.timeout = setTimeout(this.updateHeight, 16);
+    // } else {
+    //   this.setState({
+    //     previousValue: value,
+    //   });
+    // }
+  };
+
+  componentWillReceiveProps(nextProps) {
+    // const duration = parseInt(this.props.duration, 10);
+    // if (parseInt(nextProps.duration, 10) === duration) {
+    //   return;
+    // }
+    // this.setState({
+    //   previousValue: this.state.displayValue,
+    // });
+    // if (!window.isNaN(parseInt(this.props.delayValue, 10))) {
+    //   this.delayTimeout = setTimeout(() => {
+    //     this.startAnimationTime = new Date().getTime();
+    //     this.updateNumber();
+    //   }, this.props.delayValue);
+    // } else {
+    //   this.startAnimationTime = new Date().getTime();
+    //   this.updateNumber();
+    // }
+  }
+
   render() {
-    
     return this.props.render({
       onToggle: this.onToggle,
       setBoxElement: this.setBoxElement,
@@ -45,36 +117,38 @@ class ToggleBox extends Component {
     });
   }
 
-  componentDidUpdate(){
-  }
-  componentWillReceiveProps(nextProps) {}
+  componentDidUpdate() {}
+
   shouldComponentUpdate(nextProps, nextState) {
     return true;
   }
+
   componentWillUnmount() {
     clearTimeout(this.timeout);
-    clearTimeout(this.delayTimeout);
+    // clearTimeout(this.delayTimeout);
   }
 }
 
 class App extends Component {
   render() {
     return (
-      <ToggleBox 
-        
-        render={({ onToggle, setBoxElement, state }) => 
+      <ToggleBox
+        duration={1000}
+        ease="linear"
+        render={({ onToggle, setBoxElement, state }) => (
           <div className="togglebox">
             <div className="togglebox__toggle">
               <button onClick={onToggle}>toggle</button>
             </div>
             <div className="togglebox__box" ref={setBoxElement}>
-              box
+              <div className="togglebox__box-inner">
+                box inner
+              </div>
             </div>
-            <pre>{
-              JSON.stringify(state, null, 2)
-            }</pre>
+            <pre>{JSON.stringify(state, null, 2)}</pre>
           </div>
-      } />
+        )}
+      />
     );
   }
 }
